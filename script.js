2a92f070c9b03b55d210bd5002fb4752b8f3a6a2
Kingdom-Coding <jonathanv2283219@gmail.com>
2024-12-19 18:35:12 -0500

    updated scripts.js by added cross-out functionality on doubleclicking the items & adding items with pressing the enter key

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function functionality() {
   );
   const errorMessage = $("#error-message");
 
-  //add item and 
+  //add item and throw error if there's no value when adding an item
   if (inputValue === "") {
     errorMessage.css("visibility", "visible");
     $("#add-item").on("click", () => {
@@ -16,23 +16,45 @@ function functionality() {
     $("ol").append(newItem);
   }
 
-  $("#add-item").on("keydown", (e) => {
-    if (e.key === "Enter") {
-      e.preventDefault();
-
-      if ($("#add-item").val() === "") {
-        console.log("ef");
-      }
-    }
-  });
-
+  //delete item with the crossout btn
   const deleteButton = $("<crossOutButton></crossOutButton>");
   deleteButton.append(document.createTextNode("X"));
   newItem.append(deleteButton);
-
   deleteButton.on("click", () => {
     newItem.remove();
   });
 
+  //add double click feture to crossout buttons when double clicked and to uncross them
+  const doubleClick = () => {
+    if (!newItem.hasClass("strike")) {
+      newItem.addClass("strike");
+    } else {
+      newItem.removeClass("strike");
+    }
+  };
+  newItem.on("dblclick", doubleClick);
+
+  //add sorting functionality to the list
   $("#list").sortable();
 }
+
+function enterFunctionality() {
+  const inputElement = $("input");
+  const errorMessage = $("#error-message");
+
+  inputElement.on("keydown", (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      if (inputElement.val() === "") {
+        errorMessage.css("visibility", "visible");
+        $("#add-item").on("click", () => {
+          errorMessage.css("visibility", "hidden");
+        });
+      } else {
+        functionality();
+      }
+    }
+  });
+}
+
+enterFunctionality();
